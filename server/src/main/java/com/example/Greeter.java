3c7f224ba5e970726cb2755252a45d68c2be33a2
tahararib <37878662+tahararib@users.noreply.github.com>
2025-03-25 15:05:35 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,12 +18,24 @@ public class Greeter {
  public void doSomeThing() {
    // do no thing yet !
    }
+  public void doSomeThing2() {
+   // do no thing yet !
+   }
+
+  public void doSomeThing3() {
+   // do no thing yet !
+   }
 
    /**
  * @param someone name of a person
  * @return greeting string
  */
   public final String greet(final String someone) {
+   return String.format("Hello World again, %s!", someone);
+   
+ }
+
+  public final String greet2(final String someone) {
    return String.format("Hello World again, %s!", someone);
    
  }

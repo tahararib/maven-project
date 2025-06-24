e17d770494eda2a2ceebace174c444cda7b1592d
tahararib <37878662+tahararib@users.noreply.github.com>
2025-06-24 16:03:19 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -30,7 +30,9 @@ public class Greeter {
    // do no thing yet !
    }
    /**
-
+ * @param someone name of a person
+ * @return greeting string
+ */
   public final String greet(final String someone) {
    return String.format("Hello World again, %s!", someone);
    
